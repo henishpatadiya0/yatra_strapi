b6b36fd591f21e4440913105a385c98fa1238982
Henish Patadiya <henishpatadiya@websmithsolution.com>
2023-02-02 18:25:15 +0530

    Initial commit

---
+++ src/api/visa-type/services/visa-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * visa-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::visa-type.visa-type');
